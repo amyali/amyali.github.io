6b497ee114a3ecc57523ee889d2dc70e974ae1e2
Amy Li <amyli@andrew.cmu.edu>
2018-02-21 19:41:16 -0500

    added fullPage js

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		sectionsColor: ['#e7e7e7', '#F5826D', '#7BAABE', 'whitesmoke'],
+		scrollingSpeed: 1000
+
+
+	});
+  // $('.hover').mouseleave(function(){
+  //   $(this).text("IT Leadership Development Program");
+  //   $(this).css("color","#737373");
+  //   $(this).text("ITLDP").fadeIn();
+  // })
+
+});
